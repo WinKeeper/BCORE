efc68e5cb69279e082fb59b8b363171750f944a3
WinKeeper <winkeeper@mail.ru>
2026-06-06 15:57:22 +0500

    fix: добавил в контроль версий ApplicationTest и LeadControllerTest

---
+++ src/test/java/ru/mentee/power/crm/spring/ApplicationTest.java
@@ -0,0 +1,34 @@
+package ru.mentee.power.crm.spring;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ApplicationTest {
+
+  @LocalServerPort
+  private int port;
+
+  private final RestTemplate restTemplate = new RestTemplate();
+
+  @Test
+  void shouldServeLeadsPage() {
+    String url = "http://localhost:" + port + "/leads";
+    String body = restTemplate.getForEntity(url, String.class).getBody();
+
+    assertThat(body).contains("Lead List");
+  }
+
+  @Test
+  void shouldContainSeededLeads() {
+    String url = "http://localhost:" + port + "/leads";
+    String body = restTemplate.getForEntity(url, String.class).getBody();
+
+    assertThat(body).contains("NEW0@mail.ru");
+    assertThat(body).contains("CONTACTED0@mail.ru");
+  }
+}
