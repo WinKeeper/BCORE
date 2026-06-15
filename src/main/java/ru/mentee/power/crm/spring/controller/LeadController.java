9fcac653b96ec61160787a1bffcb59b3307eac42
WinKeeper <winkeeper@mail.ru>
2026-06-15 14:26:20 +0500

    feat: - LeadController добавлены два метода showEditForm и updateLead
    - LeadService добавлен метод updateLead

--- src/main/java/ru/mentee/power/crm/spring/controller/LeadController.java
+++ src/main/java/ru/mentee/power/crm/spring/controller/LeadController.java
@@ -1,14 +1,18 @@
 package ru.mentee.power.crm.spring.controller;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 import ru.mentee.power.crm.model.Lead;
 import ru.mentee.power.crm.model.LeadStatus;
 import ru.mentee.power.crm.spring.service.LeadService;
@@ -52,6 +56,23 @@ public class LeadController {
     model.addAttribute("leads", leads);
     model.addAttribute("currentFilter", status);
     return "leads/list";
+  }
+
+  @GetMapping("/leads/{id}/edit")
+  public String showEditForm(@PathVariable UUID id, Model model) {
+    if (leadService.findById(id) == null) {
+      new ResponseStatusException(HttpStatus.NOT_FOUND, "Lead not found");
+    }
+
+    model.addAttribute("lead", leadService.findById(id));
+
+    return "spring/edit";
+  }
+
+  @PostMapping("/leads/{id}")
+  public String updateLead(@PathVariable UUID id, @ModelAttribute Lead lead) {
+    leadService.updateLead(id, lead);
+    return ("redirect:/leads");
   }
 
 }
